218c0c71cfa8e61716acf5c312074ca41ad8245b
Ranjeet Kumar Yadav <138971007+Ranjeet5644@users.noreply.github.com>
2024-05-09 18:43:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+for(let i = 0 ; i< 10 ; i++){
+
+    console.log(i);
+
+}
+
+let obj = {
+   Name : "Ranjeet",
+   Role:"Programmer",
+   company:"xyz"
+}
+
+for(const key in  obj){
+         console.log(key)
+}
+
+
+for(let c of "Ranjeet"){
+
+    console.log(c)
+
+}
+
+
+let i = 0;
+
+while(i<6){
+
+console.log(i)
+i++
+
+}
